84e9b408efe156f5e6e269a6cff942d1c81376e3
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-06-09 19:01:51 +0200

    Add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,41 @@
+import { Schema } from 'mongoose';
+
+const mongoose = require('mongoose');
+
+interface INotification {
+  date: Date;
+  text: string;
+  read: boolean;
+  depositID: Schema.Types.ObjectId;
+}
+
+export interface IUser {
+  email: string;
+  passwordHash: number;
+  passwordSalt: number;
+  name: string;
+  surname: string;
+  cf: string;
+  address: string;
+  notifications: INotification[];
+}
+
+module.exports = function () {
+  const NotificationSchema = new Schema<INotification>({
+    date: { type: Date, required: true },
+    text: { type: String, required: true },
+    read: { type: Boolean, required: true },
+    // depositID
+  });
+  const UserSchema = new Schema<IUser>({
+    email: { type: String, required: true, unique: true, lowercase: true },
+    passwordHash: { type: Number, required: true },
+    passwordSalt: { type: Number, required: true },
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    cf: { type: String, required: true },
+    address: { type: String, required: true },
+    notifications: { type: [NotificationSchema], required: false },
+  });
+  return mongoose.model('User', UserSchema);
+};
