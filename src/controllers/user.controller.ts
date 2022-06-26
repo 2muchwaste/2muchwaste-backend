8988d936cc38e80d24a218c15ab4e39ddd2ddd31
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-06-26 15:36:13 +0200

    Add getUserByID

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,9 +4,19 @@ import { UserModel } from '../models/user.model';
 const mongoose = require('mongoose');
 const User = require('../models/user.model.ts')(mongoose);
 
-exports.list_users = function (req: Request, res: Response) {
+exports.getUsers = function (req: Request, res: Response) {
   User.find({}, function (err: String, user: UserModel) {
     if (err) res.send(err);
     res.json(user);
+  });
+};
+
+exports.getUserByID = function (req: Request, res: Response) {
+  User.findById(req.params.id, function (err: String, user: UserModel) {
+    if (err) res.send(err);
+    else {
+      if (user == null) res.status(404).send('User not found');
+      else res.json(user);
+    }
   });
 };
