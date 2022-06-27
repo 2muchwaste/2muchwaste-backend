b9b7f8f92bed727fa435d6e8adf6c2039ce3851d
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-06-27 15:03:47 +0200

    Add area array model

---
+++ src/models/areas.model.ts
@@ -0,0 +1,32 @@
+import { Schema } from 'mongoose';
+
+const mongoose = require('mongoose');
+
+interface DistrictModel {
+  name: string;
+  addresses: string[];
+}
+
+interface DistrictsModel {
+  zipCode: number;
+  districts: DistrictModel[];
+}
+
+interface AreasModel {
+  areas: DistrictsModel[];
+}
+
+module.exports = function () {
+  const DistrictSchema = new Schema<DistrictModel>({
+    name: { type: String, required: true },
+    addresses: { type: [String], required: true },
+  });
+  const DistrictsSchema = new Schema<DistrictsModel>({
+    zipCode: { type: Number, required: true },
+    districts: { type: [DistrictSchema], required: true },
+  });
+  const AreasSchema = new Schema<AreasModel>({
+    areas: { type: [DistrictsSchema], required: true },
+  });
+  return mongoose.model('Areas', AreasSchema);
+};
