23b78c7e06bf2cc398fc43a4a964ce29b6bada20
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-06-30 12:28:11 +0200

    Refactor operator.controller.ts

--- src/controllers/operator.controller.ts
+++ src/controllers/operator.controller.ts
@@ -5,59 +5,61 @@ import { CustomerModel } from '../models/customer.model';
 const mongoose = require('mongoose');
 const Operator = require('../models/operator.model')(mongoose);
 
-exports.getOperators = function (req: Request, res: Response) {
-  Operator.find({}, function (err: String, operator: OperatorModel) {
-    if (err) res.send(err);
-    res.json(operator);
-  });
-};
-
-exports.getOperatorByID = function (req: Request, res: Response) {
-  Operator.findById(
-    req.params.id,
-    function (err: String, operator: OperatorModel) {
+export default class OperatorController {
+  getOperators = (req: Request, res: Response) => {
+    Operator.find({}, function (err: String, operator: OperatorModel) {
       if (err) res.send(err);
-      else {
-        if (operator == null) res.status(404).send('Operator not found');
-        else res.json(operator);
+      res.json(operator);
+    });
+  };
+
+  getOperatorByID = (req: Request, res: Response) => {
+    Operator.findById(
+      req.params.id,
+      function (err: String, operator: OperatorModel) {
+        if (err) res.send(err);
+        else {
+          if (operator == null) res.status(404).send('Operator not found');
+          else res.json(operator);
+        }
       }
-    }
-  );
-};
+    );
+  };
 
-exports.createOperator = function (req: Request, res: Response) {
-  const new_user = new Operator(req.body);
-  new_user.save(function (err: String, user: CustomerModel) {
-    if (err) res.send(err);
-    res.status(201).json(user);
-  });
-};
-
-exports.updateOperator = function (req: Request, res: Response) {
-  Operator.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    { new: true },
-    function (err: String, operator: OperatorModel) {
+  createOperator = (req: Request, res: Response) => {
+    const new_user = new Operator(req.body);
+    new_user.save(function (err: String, user: CustomerModel) {
       if (err) res.send(err);
-      else {
-        if (operator == null) res.status(404).send('Operator not found');
-        else res.json(operator);
-      }
-    }
-  );
-};
+      res.status(201).json(user);
+    });
+  };
 
-exports.deleteOperator = function (req: Request, res: Response) {
-  Operator.deleteOne(
-    { _id: req.params.id },
-    function (err: String, result: { deletedCount: number }) {
-      if (err) res.send(err);
-      else {
-        if (result.deletedCount == 0)
-          res.status(404).send('Operator not found');
-        else res.json('Operator successfully deleted');
+  updateOperator = (req: Request, res: Response) => {
+    Operator.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true },
+      function (err: String, operator: OperatorModel) {
+        if (err) res.send(err);
+        else {
+          if (operator == null) res.status(404).send('Operator not found');
+          else res.json(operator);
+        }
       }
-    }
-  );
-};
+    );
+  };
+
+  deleteOperator = (req: Request, res: Response) => {
+    Operator.deleteOne(
+      { _id: req.params.id },
+      function (err: String, result: { deletedCount: number }) {
+        if (err) res.send(err);
+        else {
+          if (result.deletedCount == 0)
+            res.status(404).send('Operator not found');
+          else res.json('Operator successfully deleted');
+        }
+      }
+    );
+  };
+}
