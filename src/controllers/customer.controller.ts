edba2c1c0b28ee865a7809a791a0a210790741f1
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-06-30 16:53:09 +0200

    Create Controller Factory to DRY queries like create, delete, updateByID, getByID and getAll

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,64 +1,67 @@
-import { Request, Response } from 'express';
 import { CustomerModel } from '../models/customer.model';
+import ControllerFactory from '../utils/controller.factory';
 
 const mongoose = require('mongoose');
 const Customer = require('../models/customer.model')(mongoose);
+const controller = new ControllerFactory<CustomerModel>('Customer');
 
 export default class CustomerController {
-  getCustomers = (req: Request, res: Response) => {
-    Customer.find({}, function (err: String, user: CustomerModel) {
-      if (err) res.send(err);
-      res.json(user);
-    });
-  };
+  getCustomers = controller.getAll(Customer);
+  // getCustomers = (req: Request, res: Response) => {
+  //   Customer.find({}, function (err: String, user: CustomerModel) {
+  //     if (err) res.send(err);
+  //     res.json(user);
+  //   });
+  // };
 
-  getCustomerByID = (req: Request, res: Response) => {
-    Customer.findById(
-      req.params.id,
-      function (err: String, user: CustomerModel) {
-        if (err) res.send(err);
-        else {
-          if (user == null) res.status(404).send('Customer not found');
-          else res.json(user);
-        }
-      }
-    );
-  };
-
-  updateCustomer = (req: Request, res: Response) => {
-    Customer.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      { new: true },
-      function (err: String, user: CustomerModel) {
-        if (err) res.send(err);
-        else {
-          if (user == null) res.status(404).send('Customer not found');
-          else res.json(user);
-        }
-      }
-    );
-  };
-
-  createCustomer = (req: Request, res: Response) => {
-    const new_customer = new Customer(req.body);
-    new_customer.save((err: String, customer: CustomerModel) => {
-      if (err) res.send(err);
-      res.status(201).json(customer);
-    });
-  };
-
-  deleteCustomer = (req: Request, res: Response) => {
-    Customer.deleteOne(
-      { _id: req.params.id },
-      function (err: String, result: { deletedCount: number }) {
-        if (err) res.send(err);
-        else {
-          if (result.deletedCount == 0)
-            res.status(404).send('Customer not found');
-          else res.json('Customer successfully deleted');
-        }
-      }
-    );
-  };
+  getCustomerByID = controller.getByID(Customer);
+  // getCustomerByID = (req: Request, res: Response) => {
+  //   Customer.findById(
+  //     req.params.id,
+  //     function (err: String, user: CustomerModel) {
+  //       if (err) res.send(err);
+  //       else {
+  //         if (user == null) res.status(404).send('Customer not found');
+  //         else res.json(user);
+  //       }
+  //     }
+  //   );
+  // };
+  updateCustomer = controller.updateByID(Customer);
+  // updateCustomer = (req: Request, res: Response) => {
+  //   Customer.findOneAndUpdate(
+  //     { _id: req.params.id },
+  //     req.body,
+  //     { new: true },
+  //     function (err: String, user: CustomerModel) {
+  //       if (err) res.send(err);
+  //       else {
+  //         if (user == null) res.status(404).send('Customer not found');
+  //         else res.json(user);
+  //       }
+  //     }
+  //   );
+  // };
+  createCustomer = controller.createOne(Customer);
+  // createCustomer = (req: Request, res: Response) => {
+  //   const new_customer = new Customer(req.body);
+  //   new_customer.save((err: String, customer: CustomerModel) => {
+  //     if (err) res.send(err);
+  //     res.status(201).json(customer);
+  //   });
+  // };
+  deleteCustomer = controller.deleteByID(Customer);
+  // deleteCustomer = (req: Request, res: Response) => {
+  //   Customer.deleteOne(
+  //     { _id: req.params.id },
+  //     function (err: String, result: { deletedCount: number }) {
+  //       if (err) res.send(err);
+  //       else {
+  //         if (result.deletedCount == 0)
+  //           res.status(404).send('Customer not found');
+  //         else res.json('Customer successfully deleted');
+  //       }
+  //     }
+  //   );
+  // };
 }
---
+++ src/utils/controller.factory.ts
@@ -0,0 +1,66 @@
+import { Model } from 'mongoose';
+import { Request, Response } from 'express';
+
+class ControllerFactory<T> {
+  docName: string;
+
+  constructor(docName: string) {
+    this.docName = docName;
+  }
+
+  getByID = (model: Model<T>) => (req: Request, res: Response) => {
+    model.findById(req.params.id, function (err: String, doc: Model<T>) {
+      if (err) res.send(err);
+      else {
+        if (doc == null) res.status(404).send('Doc not found');
+        else res.json(doc);
+      }
+    });
+  };
+
+  updateByID = (model: Model<T>) => (req: Request, _res: Response) => {
+    model.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true },
+      (err, doc, res) => {
+        if (err) res.send(err);
+        else {
+          if (doc == null) res.status(404).send('Doc not found');
+          else res.json(doc);
+        }
+      }
+    );
+  };
+
+  createOne = (model: Model<T>) => (req: Request, res: Response) => {
+    const new_doc = new model(req.body);
+    new_doc.save((err: String, doc: Model<T>) => {
+      if (err) res.send(err);
+      res.status(201).json(doc);
+    });
+  };
+
+  deleteByID = (model: Model<T>) => (req: Request, res: Response) => {
+    model.findByIdAndDelete(
+      req.params.id,
+      (err: String, result: { deletedCount: number }) => {
+        if (err) res.send(err);
+        else {
+          if (result.deletedCount == 0)
+            res.status(404).send('Customer not found');
+          else res.json('Doc successfully deleted');
+        }
+      }
+    );
+  };
+
+  getAll = (model: Model<T>) => (req: Request, res: Response) => {
+    model.find({}, (err: String, doc: Model<T>) => {
+      if (err) res.send(err);
+      res.json(doc);
+    });
+  };
+}
+
+export default ControllerFactory;
