fd0fb07b4b1dbf505636b3f80740c44a60fd8942
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-06-30 10:53:49 +0200

    Add UserSchema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,3 +1,6 @@
+import { Schema } from 'mongoose';
+import * as mongoose from 'mongoose';
+
 export interface UserModel {
   name: string;
   surname: string;
@@ -10,3 +13,19 @@ export interface UserModel {
   passwordHash: number;
   passwordSalt: number;
 }
+
+module.exports = function () {
+  const UserSchema = new Schema<UserModel>({
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    birthday: { type: Date, required: true },
+    cf: { type: String, required: true, unique: true, lowercase: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    address: { type: String, required: true },
+    zipCode: { type: Number, required: true },
+    city: { type: String, required: true },
+    passwordHash: { type: Number, required: true },
+    passwordSalt: { type: Number, required: true },
+  });
+  return mongoose.model('User', UserSchema);
+};
