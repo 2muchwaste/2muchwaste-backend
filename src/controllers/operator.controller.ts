7ac2dd97c1a337737a65a48d3154be57c87eeaa2
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-01 12:22:28 +0200

    Make OperatorController use controller factory

--- src/controllers/operator.controller.ts
+++ src/controllers/operator.controller.ts
@@ -1,65 +1,18 @@
-import { Request, Response } from 'express';
 import { OperatorModel } from '../models/operator.model';
-import { CustomerModel } from '../models/customer.model';
+import ControllerFactory from "../utils/controller.factory";
 
 const mongoose = require('mongoose');
 const Operator = require('../models/operator.model')(mongoose);
+const controller = new ControllerFactory<OperatorModel>('Operator');
 
 export default class OperatorController {
-  getOperators = (req: Request, res: Response) => {
-    Operator.find({}, function (err: String, operator: OperatorModel) {
-      if (err) res.send(err);
-      res.json(operator);
-    });
-  };
+  getOperators = controller.getAll(Operator);
 
-  getOperatorByID = (req: Request, res: Response) => {
-    Operator.findById(
-      req.params.id,
-      function (err: String, operator: OperatorModel) {
-        if (err) res.send(err);
-        else {
-          if (operator == null) res.status(404).send('Operator not found');
-          else res.json(operator);
-        }
-      }
-    );
-  };
+  getOperatorByID = controller.getByID(Operator);
 
-  createOperator = (req: Request, res: Response) => {
-    const new_user = new Operator(req.body);
-    new_user.save(function (err: String, user: CustomerModel) {
-      if (err) res.send(err);
-      res.status(201).json(user);
-    });
-  };
+  createOperator = controller.createOne(Operator);
 
-  updateOperator = (req: Request, res: Response) => {
-    Operator.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      { new: true },
-      function (err: String, operator: OperatorModel) {
-        if (err) res.send(err);
-        else {
-          if (operator == null) res.status(404).send('Operator not found');
-          else res.json(operator);
-        }
-      }
-    );
-  };
+  updateOperator = controller.updateByID(Operator);
 
-  deleteOperator = (req: Request, res: Response) => {
-    Operator.deleteOne(
-      { _id: req.params.id },
-      function (err: String, result: { deletedCount: number }) {
-        if (err) res.send(err);
-        else {
-          if (result.deletedCount == 0)
-            res.status(404).send('Operator not found');
-          else res.json('Operator successfully deleted');
-        }
-      }
-    );
-  };
+  deleteOperator = controller.deleteByID(Operator);
 }
