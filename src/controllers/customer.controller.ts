b0ae7e0358e47e6483183d81d214b5393a9abb6f
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-04 11:30:15 +0200

    Delete comments

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -7,61 +7,12 @@ const controller = new ControllerFactory<CustomerModel>('Customer');
 
 export default class CustomerController {
   getCustomers = controller.getAll(Customer);
-  // getCustomers = (req: Request, res: Response) => {
-  //   Customer.find({}, function (err: String, user: CustomerModel) {
-  //     if (err) res.send(err);
-  //     res.json(user);
-  //   });
-  // };
 
   getCustomerByID = controller.getByID(Customer);
-  // getCustomerByID = (req: Request, res: Response) => {
-  //   Customer.findById(
-  //     req.params.id,
-  //     function (err: String, user: CustomerModel) {
-  //       if (err) res.send(err);
-  //       else {
-  //         if (user == null) res.status(404).send('Customer not found');
-  //         else res.json(user);
-  //       }
-  //     }
-  //   );
-  // };
+
   updateCustomer = controller.updateByID(Customer);
-  // updateCustomer = (req: Request, res: Response) => {
-  //   Customer.findOneAndUpdate(
-  //     { _id: req.params.id },
-  //     req.body,
-  //     { new: true },
-  //     function (err: String, user: CustomerModel) {
-  //       if (err) res.send(err);
-  //       else {
-  //         if (user == null) res.status(404).send('Customer not found');
-  //         else res.json(user);
-  //       }
-  //     }
-  //   );
-  // };
+
   createCustomer = controller.createOne(Customer);
-  // createCustomer = (req: Request, res: Response) => {
-  //   const new_customer = new Customer(req.body);
-  //   new_customer.save((err: String, customer: CustomerModel) => {
-  //     if (err) res.send(err);
-  //     res.status(201).json(customer);
-  //   });
-  // };
+
   deleteCustomer = controller.deleteByID(Customer);
-  // deleteCustomer = (req: Request, res: Response) => {
-  //   Customer.deleteOne(
-  //     { _id: req.params.id },
-  //     function (err: String, result: { deletedCount: number }) {
-  //       if (err) res.send(err);
-  //       else {
-  //         if (result.deletedCount == 0)
-  //           res.status(404).send('Customer not found');
-  //         else res.json('Customer successfully deleted');
-  //       }
-  //     }
-  //   );
-  // };
 }
