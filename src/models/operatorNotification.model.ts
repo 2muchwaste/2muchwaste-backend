a2e1bf5d6e74f777aa95b5110fbf0e38ead60c48
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-06 18:47:55 +0200

    Refactor model

--- src/models/operatorNotification.model.ts
+++ src/models/operatorNotification.model.ts
@@ -2,7 +2,7 @@ import { Schema } from 'mongoose';
 
 const mongoose = require('mongoose');
 
-export interface OperatorNotificationModel {
+export interface IOperatorNotification {
   dumpsterID: Schema.Types.ObjectId;
   managedByOperator: Schema.Types.ObjectId;
   date: Date;
@@ -10,30 +10,32 @@ export interface OperatorNotificationModel {
   status: string;
 }
 
-module.exports = () => {
-  const OperatorNotificationSchema = new Schema<OperatorNotificationModel>({
-    type: {
-      type: String,
-      required: true,
-      default: DumpsterErrorTypes.ERROR,
-      enum: Object.values(DumpsterErrorTypes),
-    },
-    status: {
-      type: String,
-      required: true,
-      default: NotificationStatus.PENDING,
-      enum: Object.values(NotificationStatus),
-    },
-    dumpsterID: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'Dumpster',
-    },
-    managedByOperator: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-  });
-  return mongoose.model('OperatorNotification', OperatorNotificationSchema);
-};
+const OperatorNotificationSchema = new Schema<IOperatorNotification>({
+  type: {
+    type: String,
+    required: true,
+    default: DumpsterErrorTypes.ERROR,
+    enum: Object.values(DumpsterErrorTypes),
+  },
+  status: {
+    type: String,
+    required: true,
+    default: NotificationStatus.PENDING,
+    enum: Object.values(NotificationStatus),
+  },
+  dumpsterID: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Dumpster',
+  },
+  managedByOperator: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+});
+
+export default mongoose.model(
+  'OperatorNotification',
+  OperatorNotificationSchema
+);
