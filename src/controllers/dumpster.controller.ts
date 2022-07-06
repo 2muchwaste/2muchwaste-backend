77bf7cb40b936ef5860cf81a67e49cda55ed2373
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-06 18:47:46 +0200

    Refactor dumpster controller

--- src/controllers/dumpster.controller.ts
+++ src/controllers/dumpster.controller.ts
@@ -1,18 +1,6 @@
-import BasicService from '../services/basic.service';
-import { DumpsterModel } from '../models/dumpster.model';
+import BaseController from './base.controller';
+import { IDumpster } from '../models/dumpster.model';
 
-const mongoose = require('mongoose');
-const Dumpster = require('../models/dumpster.model')(mongoose);
-const controller = new BasicService<DumpsterModel>('Dumpster');
-
-export default class DumpsterController {
-  getDumpsters = controller.getAll(Dumpster);
-
-  getDumpsterByID = controller.getByID(Dumpster);
-
-  createDumpster = controller.createOne(Dumpster);
-
-  updateDumpster = controller.updateByID(Dumpster);
-
-  deleteDumpster = controller.deleteByID(Dumpster);
+export default class DumpsterController extends BaseController<IDumpster> {
+  // Add here custom controls
 }
--- src/routes/dumpster.routes.ts
+++ src/routes/dumpster.routes.ts
@@ -1,17 +1,18 @@
 import express, { Router } from 'express';
 import DumpsterController from '../controllers/dumpster.controller';
+import DumpsterModel from '../models/dumpster.model';
 
 const router: Router = express.Router();
-const dumpsterController = new DumpsterController();
+const dumpsterController = new DumpsterController('Dumpster');
 
 router
   .route('/')
-  .get(dumpsterController.getDumpsters)
-  .post(dumpsterController.createDumpster);
+  .get(dumpsterController.getAll(DumpsterModel))
+  .post(dumpsterController.createOne(DumpsterModel));
 router
   .route('/:id')
-  .get(dumpsterController.getDumpsterByID)
-  .put(dumpsterController.updateDumpster)
-  .delete(dumpsterController.deleteDumpster);
+  .get(dumpsterController.getByID(DumpsterModel))
+  .put(dumpsterController.updateByID(DumpsterModel))
+  .delete(dumpsterController.deleteByID(DumpsterModel));
 
 export default router;
