81b2d183cb09aef61febc093d30e1807c3ca8d8a
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-06 18:48:29 +0200

    Refactor model

--- src/models/areas.model.ts
+++ src/models/areas.model.ts
@@ -16,17 +16,16 @@ interface AreasModel {
   areas: DistrictsModel[];
 }
 
-module.exports = () => {
-  const DistrictSchema = new Schema<DistrictModel>({
-    name: { type: String, required: true },
-    addresses: { type: [String], required: true },
-  });
-  const DistrictsSchema = new Schema<DistrictsModel>({
-    zipCode: { type: Number, required: true },
-    districts: { type: [DistrictSchema], required: true },
-  });
-  const AreasSchema = new Schema<AreasModel>({
-    areas: { type: [DistrictsSchema], required: true },
-  });
-  return mongoose.model('Areas', AreasSchema);
-};
+const DistrictSchema = new Schema<DistrictModel>({
+  name: { type: String, required: true },
+  addresses: { type: [String], required: true },
+});
+const DistrictsSchema = new Schema<DistrictsModel>({
+  zipCode: { type: Number, required: true },
+  districts: { type: [DistrictSchema], required: true },
+});
+const AreasSchema = new Schema<AreasModel>({
+  areas: { type: [DistrictsSchema], required: true },
+});
+
+export default mongoose.model('Areas', AreasSchema);
