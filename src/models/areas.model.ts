784e9a7c52813bde2aca0c1b6ca37d7453f25cab
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-06 18:50:31 +0200

    Refactor model

--- src/models/areas.model.ts
+++ src/models/areas.model.ts
@@ -2,29 +2,29 @@ import { Schema } from 'mongoose';
 
 const mongoose = require('mongoose');
 
-interface DistrictModel {
+interface IDistrict {
   name: string;
   addresses: string[];
 }
 
-interface DistrictsModel {
+interface IDistricts {
   zipCode: number;
-  districts: DistrictModel[];
+  districts: IDistrict[];
 }
 
-interface AreasModel {
-  areas: DistrictsModel[];
+interface IAreas {
+  areas: IDistricts[];
 }
 
-const DistrictSchema = new Schema<DistrictModel>({
+const DistrictSchema = new Schema<IDistrict>({
   name: { type: String, required: true },
   addresses: { type: [String], required: true },
 });
-const DistrictsSchema = new Schema<DistrictsModel>({
+const DistrictsSchema = new Schema<IDistricts>({
   zipCode: { type: Number, required: true },
   districts: { type: [DistrictSchema], required: true },
 });
-const AreasSchema = new Schema<AreasModel>({
+const AreasSchema = new Schema<IAreas>({
   areas: { type: [DistrictsSchema], required: true },
 });
 
