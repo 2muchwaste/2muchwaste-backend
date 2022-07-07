c12a9b6cfbd0217857c8037c7ce421907e89f038
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-07 12:37:29 +0200

    Add options to DB

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,19 @@ import http from 'http';
 import app from './app';
 
 const DATABASE = process.env.DATABASE || 'invalid-db';
-mongoose.connect(DATABASE).then(() => console.log('Connected to database!'));
+const options = {
+  useNewUrlParser: true,
+  autoIndex: true, //this is the code I added that solved it all
+  keepAlive: true,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+  family: 4, // Use IPv4, skip trying IPv6
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(DATABASE, options)
+  .then(() => console.log('Connected to database!'))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
