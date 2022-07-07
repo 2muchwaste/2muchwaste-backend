e0cef7c70c076d374a20f65e1dcac3027e71bf06
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-07 10:50:33 +0200

    Refactor

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,13 +3,13 @@ import UserController from '../controllers/user.controller';
 import UserModel from '../models/user.model';
 
 const router: Router = express.Router();
-const userController = new UserController('User');
+const controller = new UserController('User');
 
-router.route('/').get(userController.getAll(UserModel));
+router.route('/').get(controller.getAll(UserModel));
 router
   .route('/:id')
-  .get(userController.getByID(UserModel))
-  .put(userController.updateByID(UserModel))
-  .delete(userController.deleteByID(UserModel));
+  .get(controller.getByID(UserModel))
+  .put(controller.updateByID(UserModel))
+  .delete(controller.deleteByID(UserModel));
 
 export default router;
