d01ae405368292df70d6b4d3de0c79e4cad866c1
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-19 11:39:41 +0200

    Add first implementation of operator.controller.ts

--- src/controllers/operator.controller.ts
+++ src/controllers/operator.controller.ts
@@ -1,6 +1,30 @@
 import { IOperator } from '../models/operator.model';
 import UserController from './user.controller';
+import { Model } from 'mongoose';
+import { Request, Response } from 'express';
 
 export default class OperatorController extends UserController<IOperator> {
-  // Add here custom controls
+  // TODO. Give only districts in output
+  getDistrictsFromOpEmail =
+    (model: Model<IOperator>) => async (req: Request, res: Response) => {
+      model.find(
+        { email: req.params.email },
+        (err: String, doc: Model<IOperator>) => {
+          if (err) res.send(err);
+          res.json(doc);
+        }
+      );
+    };
+  // TODO. Give only empties in output
+  getEmptiesFromOpEmail =
+    (model: Model<IOperator>) => async (req: Request, res: Response) => {
+      model.find(
+        { email: req.params.email },
+        (err: String, doc: Model<IOperator>) => {
+          if (err) res.send(err);
+          res.json(doc);
+        }
+      );
+    };
+
 }
