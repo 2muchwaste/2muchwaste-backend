242d9c34bac6fb345b782ff5546613fe894fa990
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-21 17:02:33 +0200

    Add routes

--- src/routes/operatorNotification.routes.ts
+++ src/routes/operatorNotification.routes.ts
@@ -14,5 +14,22 @@ router
   .get(controller.getByID(OperatorNotificationModel))
   .put(controller.updateByID(OperatorNotificationModel))
   .delete(controller.deleteByID(OperatorNotificationModel));
+router
+  .route('/pending')
+  .get(controller.getPendingNotifications(OperatorNotificationModel));
+router
+  .route('/problems/full')
+  .get(controller.getNotificationsByProblemFull(OperatorNotificationModel));
+router
+  .route('/problems/phy')
+  .get(controller.getNotificationsByProblemPhy(OperatorNotificationModel));
+router
+  .route('/problems/obstruction')
+  .get(
+    controller.getNotificationsByProblemObstruction(OperatorNotificationModel)
+  );
+router
+  .route('/problems/error')
+  .get(controller.getNotificationsByProblemError(OperatorNotificationModel));
 
 export default router;
