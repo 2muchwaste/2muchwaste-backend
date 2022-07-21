38415f580415c996f09001ca4628169d5e21588f
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-21 17:11:04 +0200

    Add get/set status and getUserInvoices

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,6 +1,38 @@
 import BaseController from './base.controller';
 import { IPayment } from '../models/payment.model';
+import { Model } from 'mongoose';
+import { Request, Response } from 'express';
 
 export default class PaymentController extends BaseController<IPayment> {
-  // Add here custom controls
+  getStatus = (model: Model<IPayment>) => (req: Request, res: Response) => {
+    model.findById(
+      req.params.id,
+      { status: 1, _id: 0 },
+      (err: String, doc: Model<IPayment>) => {
+        if (err) res.send(err);
+        else res.json(doc);
+      }
+    );
+  };
+  setStatus = (model: Model<IPayment>) => (req: Request, res: Response) => {
+    model.findByIdAndUpdate(
+      req.params.id,
+      { $set: { status: req.body.status } },
+      (err: String, doc: Model<IPayment>) => {
+        if (err) res.send(err);
+        else res.json(doc);
+      }
+    );
+  };
+  getUserInvoices =
+    (model: Model<IPayment>) => (req: Request, res: Response) => {
+      model.find(
+        { userID: req.params.userid },
+        { status: 1, _id: 0 },
+        (err: String, doc: Model<IPayment>) => {
+          if (err) res.send(err);
+          else res.json(doc);
+        }
+      );
+    };
 }
--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -14,5 +14,9 @@ router
   .get(controller.getByID(PaymentModel))
   .put(controller.updateByID(PaymentModel))
   .delete(controller.deleteByID(PaymentModel));
-
+router
+  .route('/:id/status')
+  .get(controller.getStatus(PaymentModel))
+  .put(controller.setStatus(PaymentModel));
+router.route('/user/:userid').get(controller.getUserInvoices(PaymentModel));
 export default router;
