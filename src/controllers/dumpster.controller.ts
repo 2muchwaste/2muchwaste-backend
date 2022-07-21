f154eb2c5b7dad285d210bce169d4c7289b2b11c
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-21 14:40:02 +0200

    Add getter/setter for availability and weight of dumpster

--- src/controllers/dumpster.controller.ts
+++ src/controllers/dumpster.controller.ts
@@ -1,6 +1,53 @@
 import BaseController from './base.controller';
 import { IDumpster } from '../models/dumpster.model';
+import { Request, Response } from 'express';
+import { Model } from 'mongoose';
 
 export default class DumpsterController extends BaseController<IDumpster> {
-  // Add here custom controls
+  getAvailability =
+    (model: Model<IDumpster>) => (req: Request, res: Response) => {
+      model.findById(
+        req.params.id,
+        { available: 1, _id: 0 },
+        (err: String, doc: Model<IDumpster>) => {
+          if (err) res.send(err);
+          else res.json(doc);
+        }
+      );
+    };
+  setAvailability =
+    (model: Model<IDumpster>) => (req: Request, res: Response) => {
+      model.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: { available: req.body.available },
+        },
+        (err: String, doc: Model<IDumpster>) => {
+          if (err) res.send(err);
+          else res.json(doc);
+        }
+      );
+    };
+  getWeight = (model: Model<IDumpster>) => (req: Request, res: Response) => {
+    model.findById(
+      req.params.id,
+      { actualWeight: 1, _id: 0 },
+      (err: String, doc: Model<IDumpster>) => {
+        if (err) res.send(err);
+        else res.json(doc);
+      }
+    );
+  };
+  setWeight = (model: Model<IDumpster>) => (req: Request, res: Response) => {
+    model.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: { actualWeight: req.body.weight },
+      },
+      (err: String, doc: Model<IDumpster>) => {
+        if (err) res.send(err);
+        else res.json(doc);
+      }
+    );
+  };
 }
--- src/routes/dumpster.routes.ts
+++ src/routes/dumpster.routes.ts
@@ -14,5 +14,13 @@ router
   .get(controller.getByID(DumpsterModel))
   .put(controller.updateByID(DumpsterModel))
   .delete(controller.deleteByID(DumpsterModel));
+router
+  .route('/:id/availability')
+  .get(controller.getAvailability(DumpsterModel))
+  .put(controller.setAvailability(DumpsterModel));
+router
+  .route('/:id/weight')
+  .get(controller.getWeight(DumpsterModel))
+  .put(controller.setWeight(DumpsterModel));
 
 export default router;
