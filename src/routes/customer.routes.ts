676f429d91cef4380ba7bc453eaa0f55db27bb8e
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-23 22:35:11 +0200

    Put auth guards

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -2,22 +2,11 @@ import express, { Router } from 'express';
 import CustomerController from '../controllers/customer.controller';
 import CustomerModel from '../models/customer.model';
 import { Roles } from '../enums/Roles';
-import CustomerAuthController from '../controllers/customerAuth.controller';
-import { verifySignUp } from '../middlewares/verifySignUp';
 import { isCustomer, verifyToken } from '../middlewares/authJwt';
 const router: Router = express.Router();
 
 const customerController = new CustomerController(Roles.CUSTOMER.toString());
-const authController = new CustomerAuthController();
-router
-  .route('/')
-  .get(customerController.getAll(CustomerModel))
-  .post(customerController.createOne(CustomerModel));
 
-router.route('/auth/signin').post(authController.signIn());
-router.route('/auth/signout').post(authController.signOut());
-router.use(verifySignUp.checkDuplicateEmail, verifySignUp.checkRoleExists);
-router.route('/auth/signup').post(authController.signUp(CustomerModel));
 router.use(verifyToken, isCustomer);
 router
   .route('/:id')
