d9dfb8f450ee642725cd59383799dc738d4395df
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-23 16:53:22 +0200

    Add function to verify roles

---
+++ src/middlewares/authJwt.ts
@@ -0,0 +1,98 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import UserModel, { IUser } from '../models/user.model';
+import RoleModel, { IRole } from '../models/role.model';
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // @ts-ignore
+  let token = req.session.token;
+  const secret = process.env.JWT_SECRET || '';
+  if (!token) return res.status(403).send({ message: 'No token provided' });
+
+  jwt.verify(token, secret, (err: any, decoded: any) => {
+    if (err) return res.status(401).send({ message: 'Unauthorized!' });
+    // @ts-ignore
+    req.userID = decoded.id;
+    next();
+  });
+};
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  // @ts-ignore
+  UserModel.findById(req.userID).exec((err, user: IUser) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    RoleModel.find(
+      { _id: { $in: user.role } },
+      (err: String, roles: [IRole]) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === 'admin') {
+            next();
+            return;
+          }
+        }
+        res.status(403).send({ message: 'Require admin role!' });
+      }
+    );
+  });
+};
+export const isOperator = (req: Request, res: Response, next: NextFunction) => {
+  // @ts-ignore
+  UserModel.findById(req.userID).exec((err, user: IUser) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    RoleModel.find(
+      { _id: { $in: user.role } },
+      (err: String, roles: [IRole]) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === 'operator') {
+            next();
+            return;
+          }
+        }
+        res.status(403).send({ message: 'Require operator role!' });
+      }
+    );
+  });
+};
+export const isCustomer = (req: Request, res: Response, next: NextFunction) => {
+  // @ts-ignore
+  UserModel.findById(req.userID).exec((err, user: IUser) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    RoleModel.find(
+      { _id: { $in: user.role } },
+      (err: String, roles: [IRole]) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === 'customer') {
+            next();
+            return;
+          }
+        }
+        res.status(403).send({ message: 'Require customer role!' });
+      }
+    );
+  });
+};
