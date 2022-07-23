f3fc2a87a58240d7008102e5c08b82c5d33257cd
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-24 00:49:28 +0200

    Add guards

--- src/routes/deposit.routes.ts
+++ src/routes/deposit.routes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 import DepositController from '../controllers/deposit.controller';
 import DepositModel from '../models/deposits.model';
+import { isCustomer, verifyToken } from '../middlewares/authJwt';
 
 const router: Router = express.Router();
 const controller = new DepositController('Deposits');
 
+router.use(verifyToken, isCustomer);
 router
   .route('/')
   .get(controller.getAll(DepositModel))
