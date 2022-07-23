5f14a83c6f4cf2bbfe0a981576f066a3de9d50f1
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-23 13:27:23 +0200

    Add function to initialize Roles

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import http from 'http';
 import app from './app';
+import RoleModel from './models/role.model';
 
 const DATABASE = process.env.DATABASE || 'invalid-db';
 const options = {
@@ -16,6 +17,25 @@ mongoose
   .connect(DATABASE, options)
   .then(() => console.log('Connected to database!'))
   .catch(err => console.log(err));
+
+const initial = () => {
+  RoleModel.estimatedDocumentCount((err: String, count: number) => {
+    if (!err && count === 0) {
+      new RoleModel({ name: 'customer' }).save(error => {
+        if (error) console.log('error:' + error);
+        else console.log('added customer to roles collection');
+      });
+      new RoleModel({ name: 'operator' }).save(error => {
+        if (error) console.log('error:' + error);
+        else console.log('added operator to roles collection');
+      });
+      new RoleModel({ name: 'admin' }).save(error => {
+        if (error) console.log('error:' + error);
+        else console.log('added admin to roles collection');
+      });
+    }
+  });
+};
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
