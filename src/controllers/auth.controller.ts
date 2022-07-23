0a3343ece7422a72d5864422bc356873f44d7204
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-23 23:56:29 +0200

    Refactor auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import RoleModel, { IRole } from '../models/role.model';
-import { Model } from 'mongoose';
 import jwt from 'jsonwebtoken';
 import { IUser } from '../models/user.model';
+import CustomerModel from '../models/customer.model';
+import OperatorModel from '../models/operator.model';
+import AdminModel from '../models/admin.model';
 
 export default class AuthController<T extends IUser> {
   private saltRounds: number = 8;
+  private getRole = (req: Request) => {
+    switch (req.params.role) {
+      case 'customer':
+        return CustomerModel;
+      case 'operator':
+        return OperatorModel;
+      case 'admin':
+        return AdminModel;
+    }
+  };
 
-  signUp = (model: Model<T>) => async (req: Request, res: Response) => {
+  signUp = () => async (req: Request, res: Response) => {
+    const model = this.getRole(req);
     RoleModel.findOne({ name: req.body.role }, (err: String, doc: IRole) => {
       const newUser = new model({
         name: req.body.name,
@@ -23,44 +36,51 @@ export default class AuthController<T extends IUser> {
         role: doc._id,
       });
       console.log(newUser);
-      newUser.save((err, doc) => {
+      newUser.save((err: any, doc: any) => {
         if (err) res.send(err);
         else res.status(201).json(doc);
       });
     });
   };
 
-  signIn = (model: Model<T>) => (req: Request, res: Response) => {
+  signIn = () => (req: Request, res: Response) => {
+    const model = this.getRole(req);
     model
       .findOne({
         email: req.body.email,
       })
       .populate('roles', '-__v')
-      .exec((err, user) => {
-        if (err) {
-          res.status(500).send({ message: err });
+      .exec(
+        (
+          err: any,
+          user: { password: string; _id: any; email: any; role: any }
+        ) => {
+          if (err) {
+            res.status(500).send({ message: err });
+          }
+          if (!user)
+            return res.status(404).send({ message: 'User not found.' });
+          const passwordIsValid = bcrypt.compareSync(
+            req.body.password,
+            user.password
+          );
+          if (!passwordIsValid)
+            return res.status(401).send({ message: 'Invalid password' });
+          const secret = process.env.JWT_SECRET || '';
+          // @ts-ignore
+          req.session.token = jwt.sign({ _id: user._id }, secret, {
+            expiresIn: 86400, // 24 hours
+          });
+          res.status(200).send({
+            id: user._id,
+            email: user.email,
+            role: user.role,
+          });
         }
-        if (!user) return res.status(404).send({ message: 'User not found.' });
-        const passwordIsValid = bcrypt.compareSync(
-          req.body.password,
-          user.password
-        );
-        if (!passwordIsValid)
-          return res.status(401).send({ message: 'Invalid password' });
-        const secret = process.env.JWT_SECRET || '';
-        // @ts-ignore
-        req.session.token = jwt.sign({ _id: user._id }, secret, {
-          expiresIn: 86400, // 24 hours
-        });
-        res.status(200).send({
-          id: user._id,
-          email: user.email,
-          role: user.role,
-        });
-      });
+      );
   };
 
-  signOut = (_model: Model<T>) => async (req: Request, res: Response) => {
+  signOut = () => async (req: Request, res: Response) => {
     try {
       // @ts-ignore
       req.session = null;
