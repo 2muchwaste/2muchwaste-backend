5f73acc7fc2599ceae3bc9fbcdc2f55a2361cb02
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-24 00:47:34 +0200

    Add guards

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -2,23 +2,33 @@ import express, { Router } from 'express';
 import CustomerController from '../controllers/customer.controller';
 import CustomerModel from '../models/customer.model';
 import { Roles } from '../enums/Roles';
-import { isCustomer, verifyToken } from '../middlewares/authJwt';
+import { isAdmin, isCustomer, verifyToken } from '../middlewares/authJwt';
 const router: Router = express.Router();
 
 const customerController = new CustomerController(Roles.CUSTOMER.toString());
 
 router.use(verifyToken, isCustomer);
+const handlers = [verifyToken, isCustomer];
+router.get('/:id', handlers, customerController.getByID(CustomerModel));
+router.put('/:id', handlers, customerController.updateByID(CustomerModel));
+router.delete('/:id', handlers, customerController.deleteByID(CustomerModel));
+router.get(
+  '/:cf/notifications',
+  handlers,
+  customerController.getNotifications(CustomerModel)
+);
+router.post(
+  '/:cf/notifications',
+  handlers,
+  customerController.addNotification(CustomerModel)
+);
+router.put(
+  '/:cf/notifications/:id',
+  handlers,
+  customerController.readNotification(CustomerModel)
+);
 router
-  .route('/:id')
-  .get(customerController.getByID(CustomerModel))
-  .put(customerController.updateByID(CustomerModel))
-  .delete(customerController.deleteByID(CustomerModel));
-router
-  .route('/:cf/notifications')
-  .get(customerController.getNotifications(CustomerModel))
-  .post(customerController.addNotification(CustomerModel));
-router
-  .route('/:cf/notifications/:id')
-  .put(customerController.readNotification(CustomerModel));
-
+  .use(verifyToken, isAdmin)
+  .route('/')
+  .get(customerController.getAll(CustomerModel));
 export default router;
