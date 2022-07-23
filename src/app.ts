b2c3a1f72d5979fce7da20e96311a26be30c0d98
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-23 23:56:43 +0200

    Add generic auth routes

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(
 app.use(helmet());
 app.use(express.static('public', { maxAge: 86400000 }));
 
-app.use('api/v1/auth/', authRoutes);
+app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/dumpsters', dumpsterRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/customers', customerRoutes);
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,18 +1,13 @@
 import express, { Router } from 'express';
 import AuthController from '../controllers/auth.controller';
-import CustomerModel from '../models/customer.model';
-import OperatorModel from '../models/operator.model';
 import { verifySignUp } from '../middlewares/verifySignUp';
 
 const router: Router = express.Router();
 const controller = new AuthController();
 
-router.route('/customer/signin').post(controller.signIn(CustomerModel));
-router.route('/customer/signout').post(controller.signOut(CustomerModel));
-router.route('/operator/signin').post(controller.signIn(OperatorModel));
-router.route('/operator/signout').post(controller.signIn(OperatorModel));
+router.route('/:role/signin').post(controller.signIn());
+router.route('/:role/signout').post(controller.signOut());
 router.use(verifySignUp.checkDuplicateEmail, verifySignUp.checkRoleExists);
-router.route('/customer/signup').post(controller.signUp(CustomerModel));
-router.route('/operator/signup').post(controller.signUp(OperatorModel));
+router.route('/:role/signup').post(controller.signUp());
 
 export default router;
