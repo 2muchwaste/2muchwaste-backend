f90b460545d73fdfbf7933ea24871a63c4c5d79c
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-23 18:13:40 +0200

    add guards

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import UserModel from '../models/user.model';
+import { isAdmin, verifyToken } from '../middlewares/authJwt';
 
 const router: Router = express.Router();
 const controller = new UserController('User');
 
+router.use(verifyToken, isAdmin);
 router.route('/').get(controller.getAll(UserModel));
 router
   .route('/:id')
