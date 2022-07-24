5f48916a565e10c5b0cd2d5cc8b3e9584e9f7800
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-24 15:33:57 +0200

    Add user to Request

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,11 @@
-import { Request } from 'express';
+import { IUser } from '../models/user.model';
 
 declare global {
   namespace Express {
     export interface Request {
       session?: any;
       userID: string;
+      user?: IUser;
     }
   }
 }
