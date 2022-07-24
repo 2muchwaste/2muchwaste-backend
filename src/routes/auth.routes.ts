99ed1800770a25bd9e361ecb31e1ad16339076e5
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-24 12:45:10 +0200

    Refactor guards

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,6 +5,11 @@ import { signUpHandlers } from '../utils/constants';
 const router: Router = express.Router();
 const controller = new AuthController();
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  next();
+});
+
 router.route('/:role/signin').post(controller.signIn());
 router.route('/:role/signout').post(controller.signOut());
 router.post('/:role/signup', signUpHandlers, controller.signUp());
