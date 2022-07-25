fa6bc64991c3c2602c20b0dc3d6ab8034e388f2a
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-07-25 19:58:36 +0200

    Add getall (for admin) route

--- src/routes/operator.routes.ts
+++ src/routes/operator.routes.ts
@@ -2,11 +2,12 @@ import express, { Router } from 'express';
 import OperatorController from '../controllers/operator.controller';
 import { Roles } from '../enums/Roles';
 import OperatorModel from '../models/operator.model';
-import { operatorHandlers } from '../utils/constants';
+import { adminHandlers, operatorHandlers } from '../utils/constants';
 
 const router: Router = express.Router();
 const controller = new OperatorController(Roles.OPERATOR.toString());
 
+router.get('/', adminHandlers, controller.getAll(OperatorModel));
 router.use(operatorHandlers);
 router
   .route('/:id')
