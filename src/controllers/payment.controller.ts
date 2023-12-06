f0da37277a26c3feea322156db7bcb1c390d5ad1
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-12-06 10:47:59 +0100

    Modify payment controller to send as res all object payment

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -44,7 +44,7 @@ export default class PaymentController extends BaseController<IPayment> {
     (model: Model<IPayment>) => (req: Request, res: Response) => {
       model.find(
         { userID: req.params.userid },
-        { status: 1, _id: 0 },
+        // { status: 1, _id: 0 },
         (err: String, doc: Model<IPayment>) => {
           if (err) res.send(err);
           else res.json(doc);
