ded43785b887afaa142e65ae9588b473000bcd80
Angelo Tinti <angelo.tinti.30@gmail.com>
2024-01-31 19:39:29 +0100

    Add to create deposit checker for possible full dumpster, so create operator notification if full

--- src/controllers/deposit.controller.ts
+++ src/controllers/deposit.controller.ts
@@ -2,9 +2,15 @@ import BaseController from './base.controller';
 import DepositsModel, { IDeposit } from '../models/deposits.model';
 import { Model } from 'mongoose';
 import { Request, Response } from 'express';
+import OperatorNotificationModel, {
+  IOperatorNotification,
+} from '../models/operatorNotification.model';
 import CostModel, { ICost } from '../models/cost.model';
 import DumpsterModel, { IDumpster } from '../models/dumpster.model';
 import CustomerModel, { ICustomer } from '../models/customer.model';
+import { DumpsterErrorTypes } from '../enums/DumpsterErrorTypes';
+import { NotificationStatus } from '../enums/NotificationStatus';
+import DumpsterService from '../services/dumpster.service';
 
 export default class DepositController extends BaseController<IDeposit> {
   createDeposit =
@@ -13,6 +19,28 @@ export default class DepositController extends BaseController<IDeposit> {
         req.body.dumpsterID,
         { $inc: { actualWeight: req.body.quantity } },
         (dumpErr: String, dumpDoc: IDumpster) => {
+          if (((dumpDoc.actualWeight + req.body.quantity) / dumpDoc.maxWeight) >= dumpDoc.limitUsablePercentage / 100) {
+            console.log("Dumpster full after this deposit, set false availability")
+            const newOperatorNotification = new OperatorNotificationModel({
+              dumpsterID: req.body.dumpsterID,
+              date: new Date().toISOString(),
+              type: DumpsterErrorTypes.FULL,
+              status: NotificationStatus.PENDING,
+            });
+            newOperatorNotification.save((err: String, doc: Model<IOperatorNotification>) => {
+              if (err) console.log(err)
+            });
+            DumpsterModel.findByIdAndUpdate(
+              req.body.dumpsterID,
+              {
+                $set: { available: false },
+              },
+              { new: true },
+              (dumpErr: string, dumpDoc: IDumpster) => {
+                if (dumpErr) console.log(dumpErr);
+              }
+            )
+          }
           CostModel.findOne(
             { type: dumpDoc.type },
             { pricePerKilogram: 1, _id: 0 },
@@ -28,7 +56,6 @@ export default class DepositController extends BaseController<IDeposit> {
                 userID: req.body.userID,
               });
               newDoc.save((err: String, doc: Model<IDeposit>) => {
-                console.log('save:' + doc);
                 if (err) res.send(err);
                 let text =
                   'Deposited ' +
@@ -60,6 +87,7 @@ export default class DepositController extends BaseController<IDeposit> {
                       // Send the user, with new notification
                       io.emit(io['userslogged'].get(req.body.userID), _custDoc)
                       res.status(201).json(doc);
+
                     }
                   }
                 );
